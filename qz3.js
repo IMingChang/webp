c6805902d768fa4bc141de8f18496d67f19a4b9c
b0929031 <100284597+b0929031@users.noreply.github.com>
2022-04-04 23:03:56 +0800

    Delete qz3.js

--- qz3.js
+++
@@ -1,7 +0,0 @@
-const log = document.getElementById('log3');
-log.innerHTML ="test";
-document.addEventListener("keypress",logKey);
-function logKey(e) {
-    console.log(e.keyCode);
-    log.textContent+= String.fromCharCode(e.keyCode);
-}
